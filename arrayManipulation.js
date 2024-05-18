3a0e45c5ff294fe737ef43f64964b8405a899272
theGeniusDennis <danim005@st.ug.edu.gh>
2024-05-18 03:22:22 -0700

    I made some modifications to the arrayManipulation.js file by adding export statements.

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,10 +1,11 @@
-//the processArray function
+//Tack 1: Function to process array
+
 function processArray(numbers){
     return numbers.map(num => num % 2 === 0 ? num ** 2 : num * 3);
 }
 
 
-//the formatArrayStrings function
+//Task 2:  Functin to format array strings
 function formatArrayStrings(strings, processedNums){
     return strings.map((str, index) => {
         if (processedNums[index] % 2 === 0) {
@@ -20,3 +21,7 @@ function formatArrayStrings(strings, processedNums){
 const processedNums = processArray([1,2,3,4,5]);
 const strings = ["Hello", "World" ,"JavaScript", "is", "fun"];
 console.log(formatArrayStrings(strings, processedNums));
+
+
+//Export the functions
+module.exports = { processArray, formatArrayStrings};
